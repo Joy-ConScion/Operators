d02a4d2ae722676c43bacc48684db4394688c1cb
Joy-ConScion <chris.torres2001@outlook.com>
2026-04-09 13:31:56 -0500

    Step 1 Complete

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -2,6 +2,12 @@ package com.pluralsight;
 
 public class VariableApp {
     public static void main(String[] args) {
+String favColor = "Purple";
+short yuuYearStart = 2026;
+char middleInitial = "L";
+boolean havePets = true;
+String niceMessage = "Everyone wants to enter Heaven but no one wants to die for it...";
+
 
 
     }
