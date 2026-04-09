c89d264113c46e65859c289f3721916273621dd0
Joy-ConScion <chris.torres2001@outlook.com>
2026-04-09 13:45:04 -0500

    Step 1.1 Complete

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -3,11 +3,15 @@ package com.pluralsight;
 public class VariableApp {
     public static void main(String[] args) {
 String favColor = "Purple";
+        System.out.println("What's your favorite color? " + favColor);
 short yuuYearStart = 2026;
-char middleInitial = "L";
+        System.out.println("What year did you start Year Up United? " + yuuYearStart);
+char middleInitial = 'L';
+        System.out.println("What is your middle initial? " + middleInitial);
 boolean havePets = true;
+        System.out.println("Do you have any pets? True or false? " +  havePets);
 String niceMessage = "Everyone wants to enter Heaven but no one wants to die for it...";
-
+        System.out.println("Could you provide us with a message or saying? " +  niceMessage);
 
 
     }
