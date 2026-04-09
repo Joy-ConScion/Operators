02e6c930b5c17644608a7a8b48d4a9b425f6d0e4
Joy-ConScion <chris.torres2001@outlook.com>
2026-04-09 13:57:23 -0500

    Step 2 Complete

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -13,6 +13,17 @@ boolean havePets = true;
 String niceMessage = "Everyone wants to enter Heaven but no one wants to die for it...";
         System.out.println("Could you provide us with a message or saying? " +  niceMessage);
 
+        System.out.println("Next Step");
+
+int daysOfWeek = 7;
+        System.out.println("How many days are in a week? " + daysOfWeek);
+double coffeePrice = 4.99;
+        System.out.println("The price of coffee is $" +  coffeePrice + ".");
+char favLetter = 'U';
+        System.out.println("What is your favorite letter? " + favLetter);
+ boolean rainStatus = true;
+        System.out.println("Is it raining? " + rainStatus);
+
 
     }
 
