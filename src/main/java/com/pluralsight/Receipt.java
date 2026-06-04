263b1cfe67ca3f71910f61efca2d0f5aec89c1f9
Joy-ConScion <chris.torres2001@outlook.com>
2026-06-04 12:14:11 -0500

    Add step 3 - Exercise Completed

---
+++ src/main/java/com/pluralsight/Receipt.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class Receipt {
+
+    public static void main(String[] args) {
+
+        String item = "Yo-Yo";
+        double itemPrice = 4.99;
+        int quantity = 3;
+
+        System.out.println("You bought " + quantity + " "+ item + "'s for " + itemPrice);
+
+    }
+
+}
